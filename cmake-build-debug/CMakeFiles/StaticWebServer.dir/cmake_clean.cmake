8ee52d2b2bdab98db9fa6ca201500bbced52df06
hyman-zhou <604583358@qq.com>
2020-07-28 11:50:36 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/StaticWebServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StaticWebServer.dir/main.cpp.o"
+  "StaticWebServer"
+  "StaticWebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StaticWebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
